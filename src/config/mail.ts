9ebd50c99bde3cc6d3a17a87c1d90c84f848537f
luiscostalafc <luiscostalafc@gmail.com>
2021-01-23 19:38:51 -0300

    feat: create module users

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'emailvalido.com.br',
+      name: 'Luis Costa',
+    },
+  },
+} as IMailConfig;
